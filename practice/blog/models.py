8e0dc7f4a534a6ae166b544d3243056d4123a879
gpffh20 <eunbin584836@gmail.com>
2024-04-15 10:24:51 +0900

    recover

--- practice/blog/models.py
+++ practice/blog/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Blog(models.Model):
@@ -6,6 +7,8 @@ class Blog(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     content = models.TextField()
     image = models.ImageField(upload_to='blog/', null=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
+    tag = models.ManyToManyField('Tag',blank=True)
 
     class Meta:
         db_table = 'blog'
@@ -15,3 +18,27 @@ class Blog(models.Model):
     
     def summary(self):
         return self.content[:100]
+    
+#댓글
+class Comment(models.Model):
+    content = models.CharField(max_length=200)
+    created_at = models.DateTimeField(auto_now_add=True)
+    blog = models.ForeignKey(Blog, on_delete=models.CASCADE, null=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, null=True) #1대 다 관계
+    author = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
+
+    class Meta:
+        db_table = 'comment'
+
+    def __str__(self):
+        return self.content + ' | ' + str(self.author)
+    
+#태그
+class Tag(models.Model):
+    name = models.CharField(max_length=10)
+
+    class Meta:
+        db_table = 'tag'
+
+    def __str__(self):
+        return self.name
